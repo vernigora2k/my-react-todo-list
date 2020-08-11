8456c011c008150668333dfec6d54f1ef907b8e2
vernigora2k <58885741+vernigora2k@users.noreply.github.com>
2020-08-11 15:05:45 +0300

    v1.0.5temp

--- src/AddTodo/AddTodo.js
+++ src/AddTodo/AddTodo.js
@@ -2,9 +2,18 @@ import React from 'react'
 import './AddTodo.css'
 
 export default function AddTodo() {
+    function handleChange() {
+        
+    }
+
     return  (
         <form className='addForm'>
-            <input className='addForm__input' type='text'></input>
+            <input 
+                className='addForm__input' 
+                type='text'
+                onChange='handleChange'
+                >
+            </input>
             <button className='addForm__button' type='submit'>Add</button>
         </form>
     )
--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import './App.css';
 import Title from './Title/Title'
 import AddTodo from './AddTodo/AddTodo'
 import TodoList from './TodoList/TodoList'
+import { useState } from 'react'
 
 
 function App() {
+const [inputFormValue, setInputFormValue] = useState('temp')
+
   return (
     <div className="App">
       <Title/>
-      <AddTodo/>
+      <AddTodo inputFormValue={inputFormValue}
+      />
       <TodoList/>
     </div>
   );
