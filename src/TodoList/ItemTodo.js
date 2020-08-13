6ffd411df5162f77c2588626e13dc47e0836130f
vernigora2k <58885741+vernigora2k@users.noreply.github.com>
2020-08-13 16:10:51 +0300

    v1.1.4temp

--- src/TodoList/ItemTodo.js
+++ src/TodoList/ItemTodo.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import './ItemTodo.css'
+import { createPortal } from 'react-dom'
 
 export default function ItemTodo({ todo }) {
-    console.log(todo)
+    function handleClose(event) {
+        event.preventDefault()
+        console.log(event.target.parentNode.parentNode)
+        event.target.parentNode.parentNode.style.display='none'
+    }
+
     return (
-        <li>{todo}</li>
+        <li>
+            <div className='todoItem'>
+                {todo}
+                <button 
+                    className='todoItem__btnClose' 
+                    onClick={handleClose}>
+                    Close
+                </button>
+            </div>
+        </li>
     )
 }
